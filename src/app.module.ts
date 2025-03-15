f4ee1847e714204b9bc39de55b3c2e4269abbc53
Hensley Ferrari <hensley_ferrari@hotmail.com>
2025-03-14 23:19:12 -0300

    feat: implement Users module with CreateUser functionality and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/nestjs_mongodb')],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1/nestjs_mongodb'),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
