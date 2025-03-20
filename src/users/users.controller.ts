381b4b1fe84b23714a64f64fdc15f07aca1c1fce
Hensley Ferrari <hensley_ferrari@hotmail.com>
2025-03-19 22:38:58 -0300

    feat: add getUserById method to UsersController and implement user retrieval in UsersService with validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/CreateUser.dto';
+import mongoose from 'mongoose';
 
 @Controller('users')
 export class UsersController {
@@ -15,5 +23,15 @@ export class UsersController {
   @Get()
   getUsers() {
     return this.usersService.getsUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new HttpException('User not Found', 400);
+    const findUser = await this.usersService.getUserById(id);
+    if (!findUser) throw new HttpException('User not found', 404);
+
+    return findUser;
   }
 }
